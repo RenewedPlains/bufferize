b006c311320390ef6516a73120bd7c456c2c87c4
Mario Freuler <renewedplains@gmail.com>
2021-11-30 09:15:01 +0100

    initial stash for completed plugin function

--- bufferize.php
+++ bufferize.php
@@ -10,6 +10,80 @@
  */
 
 
+
+define('WYSIWYG_META_BOX_ID', 'my-editor');
+define('WYSIWYG_EDITOR_ID', 'myeditor'); //Important for CSS that this is different
+define('WYSIWYG_META_KEY', 'extra-content');
+add_action( 'edit_form_after_editor', 'wysiwyg_render_meta_box' );
+
+
+function wysiwyg_render_meta_box() {
+    global $post;
+    $meta_box_id = WYSIWYG_META_BOX_ID;
+    $editor_id = WYSIWYG_EDITOR_ID;
+    echo "<style>
+            #post-body-content {
+            width: 100%;
+            }
+            #postdivrich {
+                width: calc(50% - 10px);
+                float: left;
+            }
+            #wp-myeditor-wrap {
+            width: calc(50% - 10px);
+            float: right;
+            margin-top: 20px;
+            }
+            .wanted-title, .replacewith-title {
+            position: relative;
+            top: 30px;
+            }
+            </style>
+            <script type='text/javascript'>
+            jQuery(function($){
+                var fix_wp_editor_height = $('#mceu_30').height();
+                $('#mceu_90, #mceu_90 iframe').height(fix_wp_editor_height);
+                $('#$meta_box_id #editor-toolbar > a').click(function(){
+                    $('#$meta_box_id #editor-toolbar > a').removeClass('active');
+                    $(this).addClass('active');
+                });
+                if($('#$meta_box_id #edButtonPreview').hasClass('active')){
+                    $('#$meta_box_id #ed_toolbar').hide();
+                }
+                $('#$meta_box_id #edButtonPreview').click(function(){
+                    $('#$meta_box_id #ed_toolbar').hide();
+                });
+                $('#$meta_box_id #edButtonHTML').click(function(){
+                    $('#$meta_box_id #ed_toolbar').show();
+                });
+                $('#media-buttons a').bind('click', function(){
+                    var customEditor = $(this).parents('#$meta_box_id');
+                    if(customEditor.length > 0){
+                        edCanvas = document.getElementById('$editor_id');
+                    } else {
+                        edCanvas = document.getElementById('content');
+                    }
+                });
+            });
+            </script>";
+
+    $content = get_post_meta($post->ID, WYSIWYG_META_KEY, true);
+    the_editor($content, $editor_id);
+    echo "<div style='clear:both; display:block;'></div>";
+}
+
+add_action('save_post', 'wysiwyg_save_meta');
+function wysiwyg_save_meta()
+{
+
+    $editor_id = WYSIWYG_EDITOR_ID;
+    $meta_key = WYSIWYG_META_KEY;
+
+    if (isset($_REQUEST[$editor_id]))
+        update_post_meta($_REQUEST['post_ID'], WYSIWYG_META_KEY, $_REQUEST[$editor_id]);
+
+}
+
 function bufferize_custom_post_type() {
     $labels = array(
         'name' => _x( 'Bufferize', 'Bufferize', 'bufferize' ),
@@ -30,7 +104,7 @@ function bufferize_custom_post_type() {
         'label' => __( 'bufferize', 'bufferize' ),
         'description' => __( 'Bufferize', 'bufferize' ),
         'labels' => $labels,
-        'supports' => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
+        'supports' => array( 'title', 'editor', 'revisions' ),
         'taxonomies' => array( ),
         'hierarchical' => false,
         'public' => false,
@@ -42,7 +116,7 @@ function bufferize_custom_post_type() {
         'can_export' => true,
         'has_archive' => true,
         'exclude_from_search' => true,
-        'publicly_queryable' => false,
+        'publicly_queryable' => true,
         'capability_type' => 'post',
         'show_in_rest' => false,
     );
@@ -63,15 +137,57 @@ function bufferize_page() {
 if($_GET['post_type'] === 'bufferize' && $_SERVER['SCRIPT_NAME'] === '/wp-admin/edit.php') {
     add_action('wp_after_admin_bar_render', 'bufferize_page');
 }
+add_action( 'init', 'replacer', 0 );
 
+
+function replacer() {
+
+
+
+
+        add_filter('template_include', function($template){
+            ob_start(function($buffer){
+                $query = new WP_Query( array( 'post_type' => 'bufferize', 'post_status' => array('publish' ) ) );
+                $posts = $query->posts;
+                foreach($posts as $posty) {
+                    $buffer = str_replace($posty->post_content, get_post_meta($posty->ID, 'extra-content', true), $buffer);
+                }
+                return $buffer;
+            });
+            return $template;
+        }, 1, 2);
+
+
+
+}
+
+add_filter('admin_init','start_buffer_EN',1);
+function start_buffer_EN($template) {
+    ob_start('end_buffer_EN');
+    return $template;
+}
+function end_buffer_EN($buffer) {
+    return str_replace('<div id="wp-content-media-buttons" class="wp-media-buttons"><button type="button" id="insert-media-button" class="button insert-media add_media" data-editor="content"><span class="wp-media-buttons-icon"></span> Add Media</button></div>','<h1 class="wanted-title">Gesucht</h1>', $buffer);
+}
+add_filter('admin_init','start_buffer_d',1);
+function start_buffer_d($template) {
+    ob_start('end_buffer_d');
+    return $template;
+}
+function end_buffer_d($buffer) {
+    return str_replace('<div id="wp-myeditor-media-buttons" class="wp-media-buttons"><button type="button" class="button insert-media add_media" data-editor="myeditor"><span class="wp-media-buttons-icon"></span> Add Media</button></div>','<h1 class="replacewith-title">Ersetzen durch</h1>', $buffer);
+}
+
+
+/*
 add_filter('template_include','start_buffer_EN',1);
 function start_buffer_EN($template) {
     ob_start('end_buffer_EN');
     return $template;
 }
 function end_buffer_EN($buffer) {
-    return str_replace('<div class="wp-block-group__inner-container"><h2>Recent Posts</h2><ul class="wp-block-latest-posts__list wp-block-latest-posts"><li><a href="http://buffer.local/hello-world/">Hello world!</a></li>
-</ul></div>','I DONT KNOW', $buffer);
+    return str_replace('<div id="wp-content-media-buttons" class="wp-media-buttons"><button type="button" id="insert-media-button" class="button insert-media add_media" data-editor="content"><span class="wp-media-buttons-icon"></span> Add Media</button></div>','I DONT KNOW', $buffer);
 }
 
 
+*/
